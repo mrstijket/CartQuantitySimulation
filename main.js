5e1ec77ffca1028db027afd97c2f26371d6f8473
Mr.Stijket <mr.stijket@gmail.com>
2023-05-20 10:08:10 +0300

    Formatter the Code

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
 let cartQuantity = 0;
-	function cartUpdate(number){
-		if(number === -3){
-			cartQuantity -= 3;
-		} else if(number === -2){
-			cartQuantity -= 2;
-		} else if(number === -1){
-			cartQuantity--;
-		} else if(number === 1){
-			cartQuantity++;
-		} else if(number === 2){
-			cartQuantity += 2;
-		} else if(number === 3){
-			cartQuantity += 3;
-		} else{
-			cartQuantity=0;
-		}
+function cartUpdate(number) {
+	if (number === -3) {
+		cartQuantity -= 3;
+	} else if (number === -2) {
+		cartQuantity -= 2;
+	} else if (number === -1) {
+		cartQuantity--;
+	} else if (number === 1) {
+		cartQuantity++;
+	} else if (number === 2) {
+		cartQuantity += 2;
+	} else if (number === 3) {
+		cartQuantity += 3;
+	} else {
+		cartQuantity = 0;
+	}
 
-		showQuantity();
+	showQuantity();
+}
+function showQuantity() {
+	if (cartQuantity > 10) {
+		alert("The cart is full! (10)");
+		cartQuantity = 10;
+	} else if (cartQuantity < 0) {
+		alert("The cart is empty! (0)");
+		cartQuantity = 0;
+	} else {
+		alert(`Cart Quantity: ${cartQuantity}`);
 	}
-	function showQuantity(){
-		if(cartQuantity > 10){
-			alert("The cart is full! (10)");
-			cartQuantity = 10;
-		} else if (cartQuantity < 0){
-			alert("The cart is empty! (0)");
-			cartQuantity = 0;
-		} else {
-			alert(`Cart Quantity: ${cartQuantity}`);		
-		}
-	}
+}
